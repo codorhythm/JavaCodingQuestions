bbae10e3e7a0dfe3b8d66884a955b61e02f6311c
Pratulya Pratap <pratulyapratap@gmail.com>
2022-10-26 13:59:35 +0530

    Linear Search

--- src/arrays/LinearSearch.java
+++ src/arrays/LinearSearch.java
@@ -2,9 +2,23 @@ package arrays;
 
 public class LinearSearch {
 
+	public static int LinearSearch(int arr[], int num)
+	{
+		for(int i=0;i<arr.length; i++)
+		{
+			if(arr[i] == num)
+			{
+				return i;
+			}
+		}
+		
+		return -1;
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int arr[] = {1,3,4,5,6,4};
+		System.out.println(LinearSearch(arr, 3));
 	}
 
 }
